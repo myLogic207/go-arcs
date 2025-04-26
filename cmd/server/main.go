2d6548a1446c0d8ef98b242d01cd62d73530004a
myLogic207 <kaitlyn@mylogic.dev>
2025-04-26 15:16:23 +0200

    add(server): log file config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
+	"strings"
 	"syscall"
+	"time"
 
 	"git.mylogic.dev/homelab/go-arcs/internal/args"
 	"git.mylogic.dev/homelab/go-arcs/pkg/mappings/collector"
@@ -33,6 +38,13 @@ var (
 			Value:   "0.0.0.0",
 			Message: "The IP Address to bind to, if none specified, binds to all ipv4",
 		},
+		"log": {
+			Name:  "log",
+			Value: "console",
+			Message: `Path to log to, if path is dir 'server.log' is appended.
+If Exists, will rotate. If an error occurs, console is used as fallback.
+Special value 'console' will force console.`,
+		},
 		// args.Flag{
 		// 	Name:    "validate",
 		// 	Value:   false,
@@ -47,13 +59,54 @@ func cleanup(ctx context.Context, cancel context.CancelFunc, sigs chan os.Signal
 		select {
 		case <-ctx.Done():
 			cancel()
-			fmt.Println("System stopped")
+			log.Println("System stopped")
 			done <- true
 		case sig := <-sigs:
-			fmt.Println(sig)
+			log.Printf("Received signal %v", sig)
 			done <- true
 		}
 	}
+}
+
+func getLogFile(name string) (*os.File, error) {
+	if name == "console" {
+		return os.Stdout, errors.New("forced console logging")
+	}
+
+	logPath, err := filepath.Abs(name)
+	if err != nil {
+		return os.Stdout, err
+	}
+
+	stat, err := os.Stat(logPath)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return os.Stdout, err
+	}
+	err = nil
+	if stat.IsDir() {
+		logPath = filepath.Join(logPath, "server.log")
+		stat, err = os.Stat(logPath)
+		if err != nil && !errors.Is(err, os.ErrNotExist) {
+			return os.Stdout, err
+		}
+	}
+
+	if err == nil && stat.Size() > 0 {
+		logPathPart, ok := strings.CutSuffix(logPath, ".log")
+		//Reference Time is: 01/02 03:04:05PM 2006 MST = January 2, 2006 at 3:04:05 PM MST
+		timestamp := time.Now().Format("2006-01-02.03-04-05")
+		var logPathNew string
+		if ok {
+			logPathNew = fmt.Sprintf("%v.%v.log", logPathPart, timestamp)
+		} else {
+			logPathNew = fmt.Sprintf("%v.%v", logPathPart, timestamp)
+		}
+		// rotate old log
+		if err := os.Rename(logPath, logPathNew); err != nil {
+			return nil, err
+		}
+	}
+	return os.Create(logPath)
 }
 
 func main() {
@@ -65,6 +118,15 @@ func main() {
 	go cleanup(ctx, cancel, sigs, done)
 
 	flags, _ := args.Init(customFlags)
+	logFile, err := getLogFile(*flags["log"].(*string))
+	if err != nil {
+		log.Println("Log (file) path not found/readable, falling back to console:", err)
+		logFile = os.Stdout
+	} else {
+		defer logFile.Close()
+	}
+	log.SetOutput(logFile)
+
 	configPath := flags["config"].(*string)
 	log.Printf("Loading configs from %v", *configPath)
 	initConfigs, err := config.Load(ctx, *configPath)
@@ -95,17 +157,20 @@ func main() {
 	}
 	s := server.New(address, initConfigStore, collectorStore)
 
-	log.Print("Starting Server...")
+	log.Print("Starting Server")
 	go func() {
 		err := s.Serve(listener)
-		if err != nil {
+		if err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen failed: %v", err)
 		}
 	}()
+	log.Printf("Server listening on %v and ready to accept connections", address)
 
 	<-done
 
-	s.Shutdown(ctx)
+	if err := s.Shutdown(ctx); err != nil {
+		log.Printf("Failed to stop Server: %v", err)
+	}
+	// listener.Close()
 	cancel()
-	log.Print("Stopped")
 }
